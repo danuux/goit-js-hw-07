43e2db4d9114bb8894152ee1bb2da94f7e9c90c6
danuux <danut_laurentiu98@yahoo.com>
2024-02-21 18:45:57 +0200

    Done exercise 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+
+const gallery = document.querySelector('ul.gallery');
+const fragment = new DocumentFragment();
+
+galleryItems.forEach(item => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
+  a.href = item.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = item.preview;
+  img.alt = item.description;
+
+  a.appendChild(img);
+  li.appendChild(a);
+
+  
+  fragment.appendChild(li);
+});
+
+gallery.appendChild(fragment);
+
+
+new SimpleLightbox('ul.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
